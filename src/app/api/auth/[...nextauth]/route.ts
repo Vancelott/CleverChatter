021c5c2269a80af9e851e041c9f7e40f4ae0a817
Vancelott <ivan.angelovv2@gmail.com>
2023-11-27 15:04:47 +0200

    reverted to the old nextauth route, and removed the .ts extension from the file name

--- src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,6 +0,0 @@
-import NextAuth, { User as NextAuthUser } from "next-auth";
-import { authOptions } from "./auth";
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };

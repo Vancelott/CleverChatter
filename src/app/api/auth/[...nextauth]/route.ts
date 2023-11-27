735159f6ce218cd1c23838641169ffceebfd5114
Vancelott <ivan.angelovv2@gmail.com>
2023-11-27 14:59:01 +0200

    changed the structure of [...nextauth] and commented out the register route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,41 +1,5 @@
-import { AuthOptions } from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
-import { PrismaClient } from "@prisma/client";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth, { User as NextAuthUser } from "next-auth";
-
-interface NextAuthUserWithStringId extends NextAuthUser {
-  id: string;
-}
-
-const prisma = new PrismaClient();
-
-const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-      profile(profile) {
-        return {
-          id: profile.id.toString(),
-          name: profile.name || profile.login,
-          email: profile.email,
-          image: profile.avatar_url,
-          username: profile.login,
-        } as NextAuthUserWithStringId;
-      },
-    }),
-  ],
-  debug: process.env.NODE_ENV === "development",
-  session: {
-    strategy: "jwt",
-  },
-  jwt: {
-    secret: process.env.NEXTAUTH_JWT_SECRET,
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import { authOptions } from "./auth";
 
 const handler = NextAuth(authOptions);
 
